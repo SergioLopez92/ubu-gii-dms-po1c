2f0a617bfb0a1b452ded8f8cb2e9a3ad42f3e94d
SergioLopez92 <slb0011@alu.ubu.es>
2018-11-12 21:01:53 +0100

    comit inicial
    Comit inicial de prueba

---
+++ src/modelo/Tarea.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package modelo;
+
+/**
+ * @author Sergio
+ *
+ */
+public class Tarea {
+	private String titulo, descripcion;
+	private float coste, beneficio;
+	//-requisito:Requisito[1] (composición), -asignadoA:Miembro[0..1]
+	//param requisito
+	public Tarea() {
+	
+	}
+	
+	//asignarMiembro(MiembroDeEquipo)
+	
+	
+}
